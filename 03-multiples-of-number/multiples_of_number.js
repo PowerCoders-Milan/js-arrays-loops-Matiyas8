9280c6363f28274c5095acd832b9e65a11b06e1b
Matiyas8 <matnig7@gmail.com>
2021-06-18 16:29:03 +0200

    exersice 3 solved

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,10 @@
 // define your function here
+
+const multiplesOf = (number, limit) => {
+    const multiples = [];
+    for (let i = 1; i <= limit / number; i++) {
+        multiples[i] = number * i;
+    }
+    return multiples;
+}
+console.log(multiplesOf(2, 26));
