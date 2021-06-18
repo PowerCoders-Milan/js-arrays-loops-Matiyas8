a2975e1724abe8283e81114ba7865d8b21bdeda7
Matiyas8 <matnig7@gmail.com>
2021-06-18 12:59:55 +0200

    exersice 3 solved

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,11 @@
 // read carefully the instructions
+let myString = 'my world is big'
+console.log(myString[1]);
+let stringArray
+stringArray = myString.split(' ');
+
+console.log(stringArray);
+stringArray = stringArray.reverse();
+console.log(stringArray);
+stringArray = stringArray.join()
+console.log(stringArray);
