1f814227475f64af368beba894853972d4fb29f8
Matiyas8 <matnig7@gmail.com>
2021-06-21 14:19:00 +0200

    exersice 6 solved

--- 06-is-prime-number/is_prime_number.js
+++ 06-is-prime-number/is_prime_number.js
@@ -1,8 +1,31 @@
-// check if a number is prime or not
-const isPrime = number => {
+//* check if a number is prime or not
 
+const isPrime = number => {
+    for (var i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return number > 1;
 }
+
+
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(7));
+console.log(isPrime(9));
+console.log(isPrime(25));
+console.log(isPrime(100));
+
 
 const getPrimes = amount => {
-    
+    let primeNumber = []
+    for (var i = 1; i <= amount; i++) {
+        if (amount % i === 0) {
+            primeNumber.push(i)
+        }
+
+    }
+    return primeNumber;
 }
+console.log(getPrimes(84));
